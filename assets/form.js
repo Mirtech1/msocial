3a26b3ec0f4dc2307c66c90f1a787715675d8a9f
Mirtech Kelechi <mirtechk@gmail.com>
2021-03-22 12:28:20 +0100

    Update form.js

--- assets/form.js
+++ assets/form.js
@@ -15,39 +15,49 @@ function checkLength(){
     }
 }
 function clickBut(){
-    return (lNameCheck(), emailCheck(), uNameCheck())
+    return (lNameCheck(), emailCheck(), uNameCheck(), fNameCheck())
+}
+function fNameCheck() {
+    let lname = document.getElementById("fname").value;
+    if (lname.length >= 3) {
+        document.getElementById("fName").style.color = "green";
+        document.getElementById("fName").innerHTML = "Looks good";
+    }else{
+        document.getElementById("fName").style.color = "red";
+        document.getElementById("fName").innerHTML = "Invalid Last Name";
+        event.preventDefault()
+    }
 }
 function lNameCheck() {
     let lname = document.getElementById("lname").value;
     if (lname.length >= 3) {
-        document.getElementById("lName").style.color = "green"
-        document.getElementById("lName").innerHTML = "Looks good"
+        document.getElementById("lName").style.color = "green";
+        document.getElementById("lName").innerHTML = "Looks good";
     }else{
-        document.getElementById("lName").style.color = "red"
-        document.getElementById("lName").innerHTML = "Invalid Last Name"
+        document.getElementById("lName").style.color = "red";
+        document.getElementById("lName").innerHTML = "Invalid Last Name";
         event.preventDefault()
     }
 }
 function emailCheck() {
     let email = document.getElementById("Email").value;
     if (email.includes("@gmail.com")) {
-        document.getElementById("email").style.color = "green"
-        document.getElementById("email").innerHTML = "Looks good"
+        document.getElementById("email").style.color = "green";
+        document.getElementById("email").innerHTML = "Looks good";
     }else{
-        document.getElementById("email").style.color = "red"
-        document.getElementById("email").innerHTML = "Invalid Email"
+        document.getElementById("email").style.color = "red";
+        document.getElementById("email").innerHTML = "Invalid Email";
         event.preventDefault()
     }
 }
 function uNameCheck() {
     let username = document.getElementById("username").value;
     if (username.length >= 3) {
-            document.getElementById("uName").style.color = "green"
-            document.getElementById("uName").innerHTML = "Looks good"
+            document.getElementById("uName").style.color = "green";
+            document.getElementById("uName").innerHTML = "Looks good";
     }else{
-            document.getElementById("uName").style.color = "red"
-            document.getElementById("uName").innerHTML = "Invalid Username"
+            document.getElementById("uName").style.color = "red";
+            document.getElementById("uName").innerHTML = "Invalid Username";
             event.preventDefault()
     }
 }
-
