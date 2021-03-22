ad53e7e4171192a89ceb8b7d62a9ea4fd391f87b
Mirtech Kelechi <mirtechk@gmail.com>
2021-03-22 12:01:25 +0100

    update

---
+++ application/views/feed.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Msocial- Your feed</title>
+    <link rel="stylesheet" href="bootstrap 5/css/bootstrap.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="bg-dark">
+    <center>
+    <div class="card col-5 mt-1">
+    <div class="card-header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid">
+                    <a href="" class="navbar-brand mt-auto"><span class="text-warning big">M</span><span class="lit">social</span></a>
+            <div class="nav-item ml-auto">
+                    <a href="#"><i class=""></i></a>
+            </div>
+            <div class="nav-item ml-2">
+                <a href="#"><i class=""></i></a>
+            </div>
+            <div class="nav-item ml-2">
+                <a href="#"><i class=""></i></a>
+            </div>
+    </nav>
+    </div>
+    <div class="card-header">
+    <nav class="navbar nav-expand-lg navbar-light bg-light container-fluid">
+            <div class="nav-item">
+                <a href="#"><i class=""></i></a>
+            </div>
+            <div class="nav-item">
+                <a href="#"><i class=""></i></a>
+            </div>
+            <div class="nav-item">
+                <a href=""><i class=""></i></a>
+            </div>
+            <div class="nav-item">
+                <a href="#"><i class=""></i></a>
+            </div>
+            <div class="nav-item">
+                <a href="#"><i class=""></i></a>
+            </div>
+            <div class="nav-item">
+                <a href="#"><i class=""></i></a>
+            </div>
+    <div class="card-body">
+    </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark container-fluid">
+            <div class="nav-item mt-2">
+                <a href="#"><img src="race.jpg" alt="" class="profilePic"></a>
+            </div>
+            <div class="nav-item ml-auto">
+                <a href="#"><button class="btn btn-lg btn-dark pb-1"><p class="btlarge">Create a post</p></button></a>
+            </div>
+    </nav>
+    </div>
+        <div class="card-body">
+            <div>
+                <a href="#"></a>
+            </div>
+        </div>
+    </div>
+    </center>
+    <script src="bootstrap 5/js/bootstrap.js"></script>
+    <script src="fontawesome/js/all.js"></script>
+</body>
+</html>
